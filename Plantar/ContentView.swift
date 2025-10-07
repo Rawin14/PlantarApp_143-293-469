bec32be82cd3749a6bf7d317c4722aeb3815243b
MomoJeerapan <chirachanchaijeerapan@gmail.com>
2025-10-07 14:15:17 +0700

    Initial Commit

---
+++ Plantar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plantar
+//
+//  Created by Jeerapan Chirachanchai on 7/10/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plantar/PlantarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantarApp.swift
+//  Plantar
+//
+//  Created by Jeerapan Chirachanchai on 7/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct PlantarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
